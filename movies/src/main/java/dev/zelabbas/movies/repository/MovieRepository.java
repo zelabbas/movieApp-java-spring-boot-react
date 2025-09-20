8f543660c52f84013e1c4df7dc044a8a3c0ed1a6
zelabbas <zakariaelabbassi2002@gmail.com>
2025-09-20 17:22:27 +0100

    add the MovieRepository that will interact with the db

---
+++ movies/src/main/java/dev/zelabbas/movies/repository/MovieRepository.java
@@ -0,0 +1,14 @@
+package dev.zelabbas.movies.repository;
+
+import dev.zelabbas.movies.Movie;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+
+    Optional<Movie> findMovieByImdbId(String imdbId);
+}
