9eaf37a72431e7a7712caac057e5455732948e88
zelabbas <zakariaelabbassi2002@gmail.com>
2025-09-20 17:25:06 +0100

    add the ReviewRepository and the ReviewService that will be responsible for saving the data in db

---
+++ movies/src/main/java/dev/zelabbas/movies/repository/ReviewRepository.java
@@ -0,0 +1,10 @@
+package dev.zelabbas.movies.repository;
+
+import dev.zelabbas.movies.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
---
+++ movies/src/main/java/dev/zelabbas/movies/service/ReviewService.java
@@ -0,0 +1,35 @@
+package dev.zelabbas.movies.service;
+
+import dev.zelabbas.movies.Movie;
+import dev.zelabbas.movies.Review;
+import dev.zelabbas.movies.repository.ReviewRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Review createReview(String reviewBody, String imdbId ) {
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+
+        return review;
+    }
+
+}
